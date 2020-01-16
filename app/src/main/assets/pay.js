5421d54aea8910c988b3fcc14bc39da00305af39
bing.dong <bing.dong@future-data.cn>
2020-01-17 03:25:55 +0800

    抓取收款页面收款记录

--- app/src/main/assets/pay.js
+++ app/src/main/assets/pay.js
@@ -1,63 +1,78 @@
-toast("即将刷新6")
+toast("即将刷新7")
 //swipe(800,300,800,600,1000)
 console.show()
 bao()
 function bao(){
+    let m_str_time = ''
+    let m_str_price = ''
     try {
-        var zhangdan = text("账单").findOne(1000).parent().parent().parent().parent().parent().child(2).child(0).child(0).child(1).child(0).child(0).child(0).child(1).children()
 
-        className("ListView").scrollBackward();
+        var payMoney = text("个人收钱").findOnce().getText()
+
         sleep(5000)
+        toast('payMoney = ' + payMoney)
+        if (null != payMoney && '' != payMoney) {
+        let payment = text("收款记录").findOnce().parent().child(2).getText()
+        if (null != payment && '' != payment) {
+            m_str_time = payment.split('收款')[0]
+            let price = payment.split('收款')[1]
+            m_str_price = price.split('元')[0]
+            toast('---m_str_time = ' + m_str_time + '--- m_str_price = ' + m_str_price)
+        } else {
+            toast('无收款记录')
+        }
+    }
 //        id("as_inner_list_view").scrollUp()
     } catch (error) {
         launchApp("支付宝")
         sleep(5000)
-        text("我的").findOnce().parent().click()
+        text("首页").findOnce().parent().click()
          sleep(5000)
-         text("账单").findOnce().parent().parent().parent().parent().parent().click()
+         text("收钱").findOnce().parent().click()
          sleep(5000)
-         var zhangdan = text("账单").findOne(1000).parent().parent().parent().parent().parent().child(2).child(0).child(0).child(1).child(0).child(0).child(0).child(1).children()
-
-    }
-    let str = []
-    for(let i = 3 ; i < zhangdan.length;i++){
-
-        let mode1 = zhangdan[i].child(0).child(1).child(0).child(0).getText()  //支付方式
-        let balance = zhangdan[i].child(0).child(1).child(0).child(1).getText() //余额
-        let time = zhangdan[i].child(0).child(1).child(2).child(0).child(1).getText()  //时间
-        let data = zhangdan[i].child(0).child(1).child(2).child(0).child(0).getText() //日期
-
-
-        let itemStr = new Object()
-        itemStr.trading_time = time + ';' + data
-        itemStr.order_money = balance
-        itemStr.transfer_type = mode1
-        itemStr.reciprocal_name = ""
-        str.push(itemStr)
-        //console.log(flag+"----"+mode + "---"+"---"+balance+"---"+time + "---" + money)//支付方式
-        console.log(mode1+"---"+balance+"---"+time+"---"+data)
-        toast(mode1+"---"+balance+"---"+time+"---"+data);
+        let payment = text("收款记录").findOnce().parent().child(2).getText()
+          if (null != payment && '' != payment) {
+              m_str_time = payment.split('收款')[0]
+              let price = payment.split('收款')[1]
+              m_str_price = price.split('元')[0]
+              toast('---m_str_time = ' + m_str_time + '--- m_str_price = ' + m_str_price)
+          } else {
+            toast('无收款记录')
+          }
     }
 
-    let cdData = JSON.stringify( str );
-    toast(cdData);
-    if (str.length > 0) {
-        toast("数据传输中。。。")
-        var action="com.submit.data"
-            app.sendBroadcast(
-              {
-                action:action,
-                extras:{
-                  sumitData:cdData
-                }
-              }
-            );
-//            id("back_button").findOne().click();
-//            toast('22222222')
-            desc("返回").findOne().click();
+    toast('---m_str_time = ' + m_str_time + '--- m_str_price = ' + m_str_price)
+    if ('' != m_str_time && '' != m_str_price) {
+        let str = []
+            let itemStr = new Object()
+            itemStr.trading_time = m_str_time
+            itemStr.order_money = m_str_price
+            let transferType = new Object();
+            transferType.mSpanCount = '0'
+            transferType.mSpanData = []
+            transferType.mSpans = []
+            transferType.mText = ''
+            itemStr.transfer_type = transferType
+            itemStr.reciprocal_name = ""
+            str.push(itemStr)
+
+            let cdData = JSON.stringify( str );
+            toast(cdData);
+            if (str.length > 0) {
+               toast("数据传输中。。。")
+               var action="com.submit.data"
+               app.sendBroadcast(
+                  {
+                     action:action,
+                     extras:{
+                         sumitData:cdData
+                     }
+                  }
+               );
+            }
+    } else {
+        toast('没抓到数据')
     }
-
-
 
 }
 
